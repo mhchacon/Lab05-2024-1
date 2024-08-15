2f31ab4d2d6a2509d438eec5911ad1066765426c
Mariana Chacon <148928206+mhchacon@users.noreply.github.com>
2024-08-15 15:51:39 -0300

    Update QuickSorter.java

--- src/main/sorter/QuickSorter.java
+++ src/main/sorter/QuickSorter.java
@@ -1,13 +1,36 @@
-package main.sorter;
+package sorter;
 
 public class QuickSorter implements Sorter {
-    public int[] sort(int[] values) {
-        // quicksort implementation
-        return values;
+
+    @Override
+    public int[] sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        return array;
     }
 
-    public int partition(int[] values, int low, int high) {
-        return 0;
+    public void quickSort(int[] array, int low, int high){
+        if (low < high) {
+            int i = quickParticio(array, low, high);
+            quickSort(array, low, i - 1);
+            quickSort(array, i + 1, high);
+        }
     }
-    
+
+    public int quickParticio(int[] array, int low, int high){
+        int pivo = array[low];
+        int i = low;
+        int temp = 0;
+        for (int j = low + 1; j <= high; j++) {
+            if (array[j] <= pivo) {
+                i+=1;
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        temp = array[i];
+        array[i] = array[low];
+        array[low] = temp;
+        return i; 
+    }
 }
